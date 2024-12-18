edb2e04706fc6c9dc9ccbcfe8f2ddb3d674ad764
Husni Na'fa Mubarok <79038126+nafamubarokhusni@users.noreply.github.com>
2024-12-18 12:38:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import pickle
 # Molecular descriptor calculator
 def desc_calc():
     # Performs the descriptor calculation
-    bashCommand = "java -Xms2G -Xmx2G -Djava.awt.headless=true -jar ./PaDEL-Descriptor/PaDEL-Descriptor.jar -removesalt -standardizenitro -fingerprints -descriptortypes ./PaDEL-Descriptor/PubchemFingerprinter.xml -dir ./ -file descriptors_output.csv"
+    bashCommand = "java -Xms2G -Xmx2G -Djava.awt.headless=true -jar https://github.com/nafamubarokhusni/MALT1-bioactivity-prediction-app/tree/master/PaDEL-Descriptor/PaDEL-Descriptor/PaDEL-Descriptor.jar -removesalt -standardizenitro -fingerprints -descriptortypes ./PaDEL-Descriptor/PubchemFingerprinter.xml -dir ./ -file descriptors_output.csv"
     process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
     os.remove('molecule.smi')
